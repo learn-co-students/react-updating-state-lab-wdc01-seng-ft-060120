e18a8e602934add36581ba762a5b25e544238a9a
BartTheScrivner <dframe22@gmail.com>
2020-08-03 22:17:32 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  render() {
+    return <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+  }
+
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,41 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super ()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+  render() {
+    return <div>
+      <button onClick={this.handleBitClick} className="bitrate">BITRATE</button>
+      <button onClick={this.handleResClick} className="resolution">RESOLUTION</button>
+          </div>
+  }
+
+  handleBitClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: { resolution: '720p' }
+      }
+    })
+  }
+}
